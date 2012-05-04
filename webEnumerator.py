3e38447fb4c411a2b66018f348d37c3dc6f32d3d
Paras Nath Chaudhary <opnchaudhary@gmail.com>
2012-05-04 14:44:31 +0545

    main executable file

---
+++ webEnumerator.py
@@ -0,0 +1,33 @@
+#!/usr/sbin/python
+#enumerates websites to check what's used to make the website
+import sys,os,socket,subprocess,urllib2,urlparse,terminal
+import wp,dp,ci,joom,oc
+#urldefrag
+term=terminal.TerminalController()
+url=""
+def main():
+	if len(sys.argv)==1:
+		usage()
+	else:
+		url=sys.argv[1]		
+		if urllib2.urlopen(''+url+'').getcode()==200:
+			urlParsed=urlparse.urlparse(''+url+'')
+			#urlDefragmented=urldefrag(''+url+'')
+			indexOpen=urllib2.urlopen(''+url+'')			
+			print term.RED+"Server :"+term.GREEN+indexOpen.info()['Server']
+			indexOpen.close()
+			wp.check_wp(''+url+'')
+			dp.check_dp(''+url+'')
+			ci.check_ci(''+url+'')
+			joom.check_joom(''+url+'')
+			#oc.check_oc(''+url+'')					
+def usage():
+	print term.GREEN+"webenumerator v0.1 alpha\nUsage: "
+	print term.RED+sys.argv[0]+" url"+term.NORMAL
+	#print sys.argv[0]+" url"+" proxy"
+def banner():
+	print ""
+
+
+if __name__=="__main__":
+	main()
